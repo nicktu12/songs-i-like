d2b07eac6710ab4f4b8bb73c2a59cd7d9201e017
Nick Teets <nickteets@gmail.com>
2023-01-09 14:02:30 -0700

    local storage and update classes

--- src/app.js
+++ src/app.js
@@ -1,27 +1,31 @@
 // dom pieces
 
-title;
-description;
-ideas;
-submit;
-error;
+dom_title
+dom_description
+dom_ideas
+dom_submit
+dom_error
+
+let data_ideas = JSON.parse(localStorage.getItem('ideas')) || []
 
 // event pieces
 
-submit.addEventListener('click', e => {
+dom_submit.addEventListener('click', e => {
     e.preventDefault()
-    if (title.value.length > 0 && description.value.length > 0) {
-        addIdea(newIdea(title.value, description.value), ideas)
-        clearElements([title, description], [error])
+    if (dom_title.value.length > 0 && dom_description.value.length > 0) {
+        // addIdea(newIdea(title.value, description.value), ideas)
+        addIdea(data_ideas, newIdea(dom_title.value, dom_description.value))
+        clearElements([dom_title, dom_description], [dom_error])
     } else {
-        addError(error)
+        addError(dom_error)
     }
 })
 
-ideas.addEventListener('click', e => {
+dom_ideas.addEventListener('click', e => {
     e.preventDefault()
     if (e.target.id.includes('-delete')) {
-        deleteIdea(ideas, e.target)
+        let id = e.target.id.split('-')[0] 
+        deleteIdea(data_ideas, id)
     }
     // make star happen
     // if (e.target.id.includes('-delete')) {
@@ -35,20 +39,32 @@ const newIdea = (title, description) => {
     return {title, description, id: Date.now(), star: false}
 }
 
-// NOTE: can we curry here? Maybe with a bit more text on the card?
-const addIdea = (idea, element) => {
-    let div = document.createElement("div")
-    div.id = idea.id + '-container'
-    div.innerHTML=`<h2>${idea.title}</h2>
-    <p>${idea.description}</p>
-    <span>${idea.star ? '⭐️' : '☆'}</span>
-    <button id="${idea.id}-delete">delete</button>`
-    // maybe have to do stars in classes
-    element.appendChild(div)
+const addIdea = (ideas, idea) => {
+    ideas.push(idea)
+    sync()
+    console.log({ data_ideas })
 }
 
-const deleteIdea = (ideas, eventTarget) => {
-    Array.from(ideas.children).find(idea => idea.id.includes(eventTarget.id.split('-')[0])).remove()
+// NOTE: can we curry here? Maybe with a bit more text on the card?
+const drawIdeas = (ideas, element) => {
+    element.innerHTML = ''
+    ideas.forEach(idea => {
+        let div = document.createElement("div")
+        div.id = idea.id + '-container'
+        div.innerHTML=`<h2>${idea.title}</h2>
+        <p>${idea.description}</p>
+        <span class="${idea.star ? `${idea.id}-star` : ``}"></span>
+        <button id="${idea.id}-delete">delete</button>`
+        // maybe have to do stars in classes
+        element.appendChild(div)
+    })
+}
+
+const deleteIdea = (ideas, deleted) => {
+    console.log(ideas, deleted)
+    data_ideas = ideas.filter(idea => idea.id !== parseInt(deleted))
+    console.log({data_ideas})
+    sync()
 }
 
 const addError = (element) => {
@@ -60,3 +76,18 @@ const clearElements = (inputs, elements) => {
     inputs.forEach(input => input.value = '')
     elements.forEach(el => el.innerHTML = '')
 }
+
+const sync = () => {
+    updateDom()
+    syncToLocalStorage(data_ideas)
+}
+
+const updateDom = () => {
+    drawIdeas(data_ideas, dom_ideas)
+}
+
+const syncToLocalStorage = () => {
+    localStorage.setItem('ideas', JSON.stringify(data_ideas))
+}
+
+sync()
